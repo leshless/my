fd7618713e692ab8550fd3c260f4e9bb133b2e6f
leshless <78437336+leshless@users.noreply.github.com>
2023-09-28 19:56:32 +0300

    upd

--- cntrea/lean/10.lean
+++ cntrea/lean/10.lean
@@ -1,4 +1,2 @@
 def Array.splitAt (n : Nat) (a : Array T) : Array T × Array T :=
   Prod.mk a[:n] a[n:]
-
-#eval #[1, 2, 3, 4].splitAt 100500
--- cntrea/lean/9.lean
+++ cntrea/lean/9.lean
@@ -1,19 +1,18 @@
-partial def foo (a b res : Array Int) : Array Int :=
-match a[0]? with
+partial def foo (a b res : Array Int) (i j : Nat) : Array Int :=
+match a[i]? with
 | .none =>
-  match b[0]? with
+  match b[j]? with
   | .none => res
-  | .some y => foo a b[1:] (.push res y)
+  | .some y => foo a b (.push res y) i (j+1)
 | .some x =>
-  match b[0]? with
-  | .none => foo a[1:] b (.push res x)
+  match b[j]? with
+  | .none => foo a b (.push res x) (i+1) j
   | .some y =>
     match compare x y with
-    | .lt => foo a[1:] b (.push res x)
-    | .eq => foo a[1:] b (.push res x)
-    | .gt => foo a b[1:] (.push res y)
+    | .lt => foo a b (.push res x) (i+1) j
+    | .eq => foo a b (.push res x) (i+1) j
+    | .gt => foo a b (.push res y) i (j+1)
 
 
 def Array.merge (a b : Array Int) : Array Int :=
-foo a b .empty
-
+  foo a b .empty 0 0
